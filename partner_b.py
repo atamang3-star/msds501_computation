4bfc0ac6fd77ac240a0c6e3f20213585ce499bb3
Sophia Chung <sophiapchung@gmail.com>
2026-06-30 11:20:21 -0700

    add try/except for ValueError

--- partner_b.py
+++ partner_b.py
@@ -33,7 +33,12 @@ scores = ['88', '95', 'absent', '72', 'n/a', '84']
 valid = []
 
 for i, s in enumerate(scores):
-    score = int(s)            # <-- this line crashes on bad strings
+    try:
+        score = int(s)
+        print(f'OK: {score}')
+    except ValueError:
+        print(f'Bad at [{i}]: {s}')
+
     print(f'Score [{i}]: {score}')
     valid.append(score)
 
